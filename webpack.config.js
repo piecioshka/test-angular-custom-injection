c4bf5d085b75bbd7a429c224e240c1a7a9950248
Piotr Kowalski <piecioshka@gmail.com>
2019-05-27 09:30:50 +0200

    Deployment

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: path.join(__dirname, 'src', 'main.ts'),
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
     },
 
